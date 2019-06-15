e5308b00e716f66cb5498ae9ec09b66b7bec4b6e
Daishuai <daishuai@kedacom.com>
2019-06-15 21:00:32 +0800

    TAPD# Kafka 修改说明: springboot整合kafka 波及分析: 无 平台/产品/模块: springboot-kafka 自测结果: 自测通过 Auditors: Daishuai

---
+++ springboot-kafka/src/main/java/com/daishuai/kafka/config/KafkaConfig.java
@@ -0,0 +1,12 @@
+package com.daishuai.kafka.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author Daishuai
+ * @description Kafka配置类
+ * @date 2019/6/15 20:59
+ */
+@Configuration
+public class KafkaConfig {
+}
