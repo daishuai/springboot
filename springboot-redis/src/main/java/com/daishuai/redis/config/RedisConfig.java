e5843058a47c710f512be5fb2735ef61bf875a29
Daishuai <daishuai@kedacom.com>
2019-06-15 21:02:38 +0800

    TAPD# springboot-redis 修改说明: redis配置 波及分析: 无 平台/产品/模块: springboot 自测结果: 自测通过 Auditors: Daishuai

---
+++ springboot-redis/src/main/java/com/daishuai/redis/config/RedisConfig.java
@@ -0,0 +1,12 @@
+package com.daishuai.redis.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author Daishuai
+ * @description Redis配置类
+ * @date 2019/6/15 21:01
+ */
+@Configuration
+public class RedisConfig {
+}
