653e47e0d4a60476431125241da1a2f6f3934353
Daishuai <867390427@qq.com>
2022-08-16 23:21:09 +0800

    netty client

---
+++ springboot-netty/netty-client/src/main/java/com/daishuai/netty/client/encoder/ObjectEncoder.java
@@ -0,0 +1,40 @@
+package com.daishuai.netty.client.encoder;
+
+import com.alibaba.fastjson.JSON;
+import io.netty.buffer.ByteBufUtil;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.MessageToMessageEncoder;
+import io.netty.util.internal.ObjectUtil;
+
+import java.nio.CharBuffer;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+
+/**
+ * @author admin
+ * @version 1.0.0
+ * @description 自定义
+ * @createTime 2022-08-16 22:21:57
+ */
+public class ObjectEncoder extends MessageToMessageEncoder<Object> {
+
+    private final Charset charset;
+
+    public ObjectEncoder() {
+        this(StandardCharsets.UTF_8);
+    }
+
+    public ObjectEncoder(Charset charset) {
+        this.charset = ObjectUtil.checkNotNull(charset, "charset");
+    }
+
+    @Override
+    protected void encode(ChannelHandlerContext ctx, Object msg, List<Object> out) throws Exception {
+        if (msg == null) {
+            return;
+        }
+        CharSequence charSequence = msg instanceof CharSequence ? (CharSequence) msg : JSON.toJSONString(msg);
+        out.add(ByteBufUtil.encodeString(ctx.alloc(), CharBuffer.wrap(charSequence), charset));
+    }
+}
---
+++ springboot-netty/netty-server/src/main/java/com/daishuai/netty/server/starter/NettyServerStarter.java
@@ -0,0 +1,49 @@
+package com.daishuai.netty.server.starter;
+
+import com.daishuai.netty.server.initializer.ServerChannelInitializer;
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author admin
+ * @version 1.0.0
+ * @description 服务端启动类
+ * @createTime 2022-08-16 21:54:24
+ */
+@Component
+public class NettyServerStarter implements ApplicationRunner {
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        // 首先，netty通过ServerBootstrap启动客户端
+        ServerBootstrap server = new ServerBootstrap();
+        EventLoopGroup parentGroup = new NioEventLoopGroup();
+        EventLoopGroup childGroup = new NioEventLoopGroup();
+        // 第一步定义两个线程组，用来处理客户端通道的accept和读写事件
+        // parentGroup用来处理accept时间，childGroup用来处理通道的读写事件
+        server.group(parentGroup, childGroup);
+        // 用于构造服务端套接字ServerSocket对象，标识当服务器请求处理线程全满时，用于临时存放已完成三次握手的请求的队列的最大长度。
+        // 用来初始化服务端可连接队列
+        // 服务端处理客户端连接请求是按顺序处理的，所以同一时间只能处理一个客户端连接，多个客户端来的时候，
+        // 服务端将不能处理的客户端连接请求放在队列中等待处理，backlog参数指定了队列的大小。
+        server.option(ChannelOption.SO_BACKLOG, 128);
+        // 第二步绑定服务端通道
+        server.channel(NioServerSocketChannel.class);
+
+        // 第三步绑定handler，处理读写事件，ChannelInitializer是给通道初始化
+        server.childHandler(new ServerChannelInitializer());
+        // 第四步绑定端口
+        try {
+            ChannelFuture future = server.bind(8989).sync();
+            future.channel().closeFuture().sync();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
