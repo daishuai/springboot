d485486b279d16cf7f4c9098f5c625ec6931c064
daishuai <daishuai@kedacom.com>
2023-08-30 17:14:49 +0800

    Feign实现上传文件

---
+++ feign-file/src/main/java/com/daishuai/feign/FeignApplication.java
@@ -0,0 +1,49 @@
+package com.daishuai.feign;
+
+import com.alibaba.fastjson.JSONObject;
+import com.daishuai.feign.client.FeignFileClient;
+import com.daishuai.feign.dto.MockMultipartFile;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.io.IOUtils;
+import org.apache.http.entity.ContentType;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.annotation.Resource;
+import java.io.IOException;
+import java.net.URL;
+
+/**
+ * @author Keda
+ * @version 1.0.0
+ * @description TODO
+ * @createTime 2023年08月30日 14:50:00
+ */
+@Slf4j
+@EnableFeignClients
+@RestController
+@SpringBootApplication
+public class FeignApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FeignApplication.class, args);
+    }
+
+    @Resource
+    private FeignFileClient feignFileClient;
+
+    @GetMapping(value = "/upload")
+    public Object upload() throws IOException {
+        URL url = new URL("https://keda119.kedacom.com/ers-outside-iacs-dev/outside-server/resource/file/default/2023-08-30/b2d67b42b77845a89d8343f1979f73a3.jpg");
+        byte[] body = IOUtils.toByteArray(url);
+        // MultipartFile, name必须和上传文件接口参数名称保持一致
+        MultipartFile file = new MockMultipartFile("file", "a.jpg", ContentType.IMAGE_JPEG.getMimeType(), body);
+        JSONObject jsonObject = feignFileClient.uploadFile(file, "battleGroup", true, 6, 0.3);
+        log.info("response: {}", jsonObject);
+        return jsonObject;
+    }
+}
---
+++ feign-file/src/main/java/com/daishuai/feign/client/FeignFileClient.java
@@ -0,0 +1,46 @@
+package com.daishuai.feign.client;
+
+import com.alibaba.fastjson.JSONObject;
+import feign.Logger;
+import feign.codec.Encoder;
+import feign.form.spring.SpringFormEncoder;
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.multipart.MultipartFile;
+
+/**
+ * @author Keda
+ * @version 1.0.0
+ * @description TODO
+ * @createTime 2023年08月30日 14:50:00
+ */
+@FeignClient(name = "feign-file", url = "http://localhost:8769/", configuration = FeignFileClient.FileUploadFeignConfiguration.class)
+public interface FeignFileClient {
+
+    @PostMapping(value = "/im/file/upload", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    JSONObject uploadFile(@RequestPart("a") MultipartFile file, @RequestParam("moduleName") String moduleName,
+                          @RequestParam("needChange") Boolean needChange, @RequestParam("index") Integer index,
+                          @RequestParam("percent") Double percent);
+
+    @PostMapping(value = "/file/upload", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    JSONObject uploadFile(@RequestPart("file") MultipartFile file);
+
+    class FileUploadFeignConfiguration {
+
+        @Bean
+        @Primary
+        public Encoder feignFormEncoder() {
+            return new SpringFormEncoder();
+        }
+
+        @Bean
+        public Logger.Level feignLoggerLevel() {
+            return Logger.Level.FULL;
+        }
+    }
+}
